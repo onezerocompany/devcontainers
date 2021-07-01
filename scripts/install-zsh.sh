5e501a773bf55a6a74e92fc139042cb9c44f04a7
Luca Silverentand <luca@onezero.company>
2021-07-01 13:17:11 +0200

    fixed .zshrc bugs

--- scripts/install-zsh.sh
+++ scripts/install-zsh.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.1/zsh-in-docker.sh)" -- \
   -t robbyrussell
-echo "export PS1=\"> \""
+echo "export PS1=\"> \"" >> /home/dev/.zshrc
