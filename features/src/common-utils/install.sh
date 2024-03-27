5d60bd14fa9fc4e4f1e21caf4053ad11e9005bf0
Luca Silverentand <luca@onezero.company>
2024-03-27 17:06:32 +0100

    Update user creation and shell configuration in install.sh

--- features/src/common-utils/install.sh
+++ features/src/common-utils/install.sh
@@ -2,10 +2,10 @@
 
 # Setup user 'zero'
 USER=${USER:-"zero"}
-if [ ! -d "/home/$USER_NAME" ]; then
-  useradd -m -s /bin/bash $USER_NAME
-  echo "$USER_NAME:$USER_NAME" | chpasswd
-  usermod -aG sudo $USER_NAME
+if [ ! -d "/home/$USER" ]; then
+  useradd -m -s /bin/bash $USER
+  echo "$USER:$USER" | chpasswd
+  usermod -aG sudo $USER
 fi
 
 # Install ZSH
@@ -13,10 +13,11 @@ INSTALL_ZSH=${ZSH:-"true"}
 if [ "$INSTALL_ZSH" = "true" ]; then
   apt-get update
   apt-get install -y zsh
-  # Set ZSH as default shell
+  # Set ZSH as default shell permanently
   ZSH_DEFAULT=${ZSH_DEFAULT:-"true"}
   if [ "$ZSH_DEFAULT" = "true" ]; then
     chsh -s $(which zsh)
+    chsh -s $(which zsh) $USER
   fi
 
   # Set prompt in zshrc for 'zero' user
