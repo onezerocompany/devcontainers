df6e930dddebc2d0c587c18f2c0b90cc9392b969
Luca Silverentand <luca@onezero.company>
2024-03-28 17:04:33 +0100

    Update eza installation and zsh prompt configuration

--- features/src/common-utils/install.sh
+++ features/src/common-utils/install.sh
@@ -26,10 +26,8 @@ fi
 EZA=${EZA:-"false"}
 if [ "$EZA" = "true" ]; then
   # Install eza
-  sudo mkdir -p /etc/apt/keyrings
-  wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-  echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
-  sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+  echo "deb http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+  sudo chmod 644 /etc/apt/sources.list.d/gierens.list
   sudo apt-get update
   sudo apt-get install -y eza
   # add eza to zshrc if available
