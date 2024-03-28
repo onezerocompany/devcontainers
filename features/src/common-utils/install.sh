8a032f8b5e024942b0841499b49f856a34bd6a41
Luca Silverentand <luca@onezero.company>
2024-03-28 16:49:33 +0100

    Add common-utils, node, and bun features to .devcontainer.json

--- features/src/common-utils/install.sh
+++ features/src/common-utils/install.sh
@@ -30,8 +30,8 @@ if [ "$EZA" = "true" ]; then
   wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
   echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
   sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
-  sudo apt update
-  sudo apt install -y eza
+  sudo apt-get update
+  sudo apt-get install -y eza
   # add eza to zshrc if available
   if [ -f ~/.zshrc ]; then
     echo "Adding eza to zshrc"
