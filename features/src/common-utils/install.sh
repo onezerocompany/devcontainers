091bb40cf2c8c2c701ac523dcd747c29f34d5fdc
Luca Silverentand <luca@onezero.company>
2024-03-30 18:27:50 +0100

    Update version and fix echo command in install.sh

--- features/src/common-utils/install.sh
+++ features/src/common-utils/install.sh
@@ -57,5 +57,5 @@ MOTD=${MOTD:-"false"}
 if [ "$MOTD" = "true" ]; then
   $(dirname $0)/motd_gen.sh > /etc/motd
   chmod 644 /etc/motd
-  echo "echo \"cat -p /etc/motd\"" >> $ZSHRC
+  echo "cat -p /etc/motd" >> $ZSHRC
 fi
