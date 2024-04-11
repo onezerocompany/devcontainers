890c2227b4fb8522d0599ff67e0ba0e91618c50b
Luca Silverentand <luca@onezero.company>
2024-04-11 17:23:43 +0200

    Update devcontainer matrix in publish workflow and add arm64 support check in firebase install script

--- features/src/firebase/install.sh
+++ features/src/firebase/install.sh
@@ -6,4 +6,10 @@ if [ "$INSTALL" != "true" ]; then
   exit 0
 fi
 
+# in case we are on arm64, fail gracefully
+if [ "$(uname -m)" == "aarch64" ]; then
+  echo "Flutter is not supported on arm64 yet."
+  exit 0
+fi
+
 su $USER -c "curl -sL https://firebase.tools | bash"
