01d031dd838ce1f4b9ee62440b46d2483edcfd0d
Luca Silverentand <luca@onezero.company>
2024-06-09 15:10:16 +0200

    Fix ownership of docker socket in entrypoint.sh

--- images/dind/entrypoint.sh
+++ images/dind/entrypoint.sh
@@ -2,11 +2,13 @@
 
 sudo /usr/bin/supervisord -c /etc/supervisor/supervisord.conf -n >> /dev/null 2>&1 &
 
-
 echo "Starting docker..."
 while ! pgrep "dockerd" >/dev/null; do
   sleep 1
 done
+
+# Make zero the owner of the docker socket
+sudo chown zero:zero /var/run/docker.sock
 
 if [ -n "$TERM" ]; then
   tput cr
