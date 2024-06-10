3dbf5c6ba9b1a8d3e0ff2d2ca96c2d95dd595fd8
Luca Silverentand <luca@onezero.company>
2024-06-10 14:13:44 +0000

    chore: Update Firebase CLI version to 1.2.0 and pre-download emulators

--- features/src/firebase/install.sh
+++ features/src/firebase/install.sh
@@ -6,10 +6,16 @@ if [ "$INSTALL" != "true" ]; then
   exit 0
 fi
 
-# in case we are on arm64, fail gracefully
 if [ "$(uname -m)" == "aarch64" ]; then
-  echo "Flutter is not supported on arm64 yet."
+  echo "Firebase is not supported on arm64 yet."
   exit 0
 fi
 
 su $USER -c "curl -sL https://firebase.tools | bash"
+
+# Pre-download the emulators
+firebase setup:emulators:firestore
+firebase setup:emulators:database
+firebase setup:emulators:pubsub
+firebase setup:emulators:storage
+firebase setup:emulators:ui
